250ecfb74958647715b88c8b8ce5d74bfff88be4
swridings <27883567swridings@user.noreply.github.com>
2020-02-05 23:53:56 -0500

    Added the table that the tasks will go into

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
-// Get the modal
-var modal = document.getElementById("addTask");
+// When window loads determine if there are tasks
+var taskList = document.getElementById('task-list');
+window.onload = function() {
+    if (taskList.tBodies[0].rows.length > 0) {
+        this.taskList.style.display = 'none';
+    }
+    else {
+        document.getElementById('no-tasks').innerText = 'Currently, you have no tasks to complete.'
+    }
+}
 
-// Get the button that opens the modal
-var btn = document.getElementById("btn-add");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var btnAddTask = document.getElementById('btn-add');
+var modal = document.getElementById('form-add-task');
+var btnShowModal = document.getElementById('btn-show-modal');
+var span = document.getElementsByClassName('close')[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+btnShowModal.onclick = function() {
+  modal.style.display = 'block';
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+  modal.style.display = 'none';
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
-    modal.style.display = "none";
+    modal.style.display = 'none';
   }
 }
