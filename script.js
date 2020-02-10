536fed6b02a49098406fe584a0955ea96e030ea0
swridings <27883567swridings@user.noreply.github.com>
2020-02-10 13:55:43 -0500

    Added ability to delete completed items

--- script.js
+++ script.js
@@ -1,89 +1,162 @@
-// Startup
+// ---------------------- Page Load ---------------------- //
 window.onload = function() {
   if (localStorage.getItem("ListOfTasks") !== null) {
-    let arrCompletionStatus = localStorage.getItem("CompletionStatus").split("|");
-    let arrTaskList = getArrayOfTasks();
+    let arrCompletionStatus = localStorage.getItem('CompletionStatus').split("|");
+    let arrTaskList = localStorage.getItem("ListOfTasks").split("|");
     arrTaskList.forEach((e, i) => {
-      addTaskToTable(e, i, arrCompletionStatus);
+      loadTasksToTable(e, i, arrCompletionStatus);
     });
+
+    document.getElementById('btn-clear').className = 'btn-status-complete';
   }
   
   if (getTableRowCount() === 0) {
       document.getElementById('task-list').style.display = 'none';
       document.getElementById('no-tasks').innerText = 'Currently, you have no tasks to complete.';
+      document.getElementById('btn-clear').className = 'btn-status-incomplete';
     }
 }
 
 
-var taskList = document.getElementById('task-list');
 
-function getArrayOfTasks() {
-  let arrStoredTasks = localStorage.getItem("ListOfTasks").split("|");
-  return arrStoredTasks;
-}
+// ------------------- Table of Tasks ------------------- //
+var taskList = document.getElementById('task-list');
 
 function getTableRowCount() {
   let taskListRowCount = taskList.getElementsByTagName('tbody')[0].rows.length;
   return taskListRowCount;
 }
 
-function changeTaskStatus(cbx) {
-  let taskNumber = cbx.id.substring(4,cbx.id.length);
-  let taskStatus = cbx.checked ? 'status-complete' : 'status-incomplete';
-  
-  document.getElementById('cbx-' + taskNumber).className = taskStatus
-  document.getElementById('task-' + taskNumber).className = taskStatus;
-  // Rewrite status to local storage
-  rewriteStatusToLocalStorage(getTableRowCount());
-}
-
-function rewriteStatusToLocalStorage(taskCount) {
-  localStorage.removeItem('CompletionStatus');
-
-  let taskStatusList = '';
-  for (i = 0; i < taskCount; i++) {
-    taskStatusList = taskStatusList + document.getElementById('task-' + i).className + '|';
-  }
-
-  taskStatusList = taskStatusList.substring(0, taskStatusList.length-1); // Remove last '|'
-  console.log(taskStatusList)
-  localStorage.setItem("CompletionStatus", taskStatusList);
-}
-
-// Adds tasks to table from local storage
-function addTaskToTable(item, index, arrCompletionStatus) {
-    // When user clicks add button, add to list
+// Load tasks to table from local storage
+function loadTasksToTable(item, index, arrCompletionStatus) {
+    // Add new row with cells
     let newRow = taskList.getElementsByTagName('tbody')[0].insertRow();
     let cell0 = newRow.insertCell(0);
     let cell1 = newRow.insertCell(1);
+    let cell2 = newRow.insertCell(2);
 
-    let isTaskComplete = (arrCompletionStatus[index] == 'status-complete') ? true : false;
-    let cbxTitle = (arrCompletionStatus[index] == 'status-complete') ? 'Mark as incomplete' : 'Mark as complete';
+    addCheckbox(index, cell0, arrCompletionStatus[index])
 
-    let cbx = document.createElement("INPUT");
-    cbx.setAttribute('id', 'cbx-' + index.toString());
-    cbx.setAttribute('class', arrCompletionStatus[index]);
-    cbx.setAttribute('onclick', 'changeTaskStatus(this)');
-    cbx.setAttribute('type', 'checkbox');
-    cbx.setAttribute('title', cbxTitle);
-    cell0.append(cbx);
-    
-    
-    document.getElementById('cbx-' + index.toString()).checked = isTaskComplete;
+    addTaskToTable(index, cell1, arrCompletionStatus[index], item);
 
-    cell1.setAttribute('id', 'task-' + index.toString());
-    cell1.setAttribute('class', arrCompletionStatus[index]);
-    cell1.innerHTML = item;
+    addDeleteButton(index, cell2, arrCompletionStatus[index]);
+}
+
+// Add task text to table
+function addTaskToTable(rowNumber, cell1, status, task) {
+  cell1.setAttribute('id', 'task-' + rowNumber.toString());
+  cell1.setAttribute('class', status);
+  cell1.innerHTML = task;
 }
 
 
-// ===================== For Tesing Only ===================== //
-var clear = document.getElementById('clear')
+
+// ---------------------- Checkboxs ---------------------- //
+function addCheckbox(rowNumber, cell0, status) {
+  let isTaskComplete = (status == 'status-complete') ? true : false;
+  let cbxTitle = (status == 'status-complete') ? 'Mark as incomplete' : 'Mark as complete';
+
+  let cbx = document.createElement('INPUT');
+  cbx.setAttribute('id', 'cbx-' + rowNumber.toString());
+  cbx.setAttribute('class', status);
+  cbx.setAttribute('onclick', 'changeTaskStatus(this)');
+  cbx.setAttribute('type', 'checkbox');
+  cbx.setAttribute('title', cbxTitle);
+  cell0.append(cbx);
+  
+  document.getElementById('cbx-' + rowNumber.toString()).checked = isTaskComplete;
+}
+
+function changeTaskStatus(cbx) {
+  let taskNumber = cbx.id.substring(4,cbx.id.length);
+  // let isTaskComplete = (arrCompletionStatus[index] == 'status-complete') ? true : false;
+  let taskStatus = cbx.checked ? 'status-complete' : 'status-incomplete';
+  let cbxTitle = (status == 'status-complete') ? 'Mark as incomplete' : 'Mark as complete';
+
+  document.getElementById('cbx-' + taskNumber).className = taskStatus
+  document.getElementById('cbx-' + taskNumber).title = cbxTitle
+  document.getElementById('task-' + taskNumber).className = taskStatus;
+  document.getElementById('delete-' + taskNumber).className = 'btn-' + taskStatus;
+
+  // Rewrite status to local storage
+  rewriteLocalStorage(getTableRowCount());
+  window.location.reload(false); 
+}
+
+
+
+// -------------------- Delete Buttons -------------------- //
+function addDeleteButton(rowNumber, cell2, status) {
+  let btnDelete = document.createElement('button');
+      btnDelete.setAttribute('id', 'delete-' + rowNumber.toString());
+      btnDelete.setAttribute('class', 'btn-' + status);
+      btnDelete.setAttribute('onclick', 'deleteRow(this)');
+      
+      cell2.append(btnDelete);
+
+      document.getElementById('delete-' + rowNumber.toString()).innerText = 'Delete';
+}
+
+function deleteRow(btnDelete) {
+  let taskNumber = btnDelete.id.substring(7,btnDelete.id.length);
+  
+  rewriteLocalStorage(getTableRowCount(), taskNumber)
+  
+  window.location.reload(false); 
+}
+
+
+
+// -------------------- Local Storage -------------------- //
+
+function addNewTaskToLocalStorage() {
+  let newTask = document.getElementById('textbox-task').value;
+  if (localStorage.getItem("ListOfTasks") === null) {
+    localStorage.setItem("ListOfTasks", newTask);
+    localStorage.setItem("CompletionStatus", 'status-incomplete');
+  }
+  else {
+    localStorage.setItem("ListOfTasks", localStorage.getItem("ListOfTasks") + '|' + newTask);
+    localStorage.setItem("CompletionStatus", localStorage.getItem("CompletionStatus") + '|' + 'status-incomplete');
+  }
+}
+
+function rewriteLocalStorage(taskCount, skipDeletedTask = -1) {
+  localStorage.removeItem('CompletionStatus');
+  localStorage.removeItem('ListOfTasks');
+
+  let taskStatusList = '';
+  let taskList = '';
+  for (i = 0; i < taskCount; i++) {
+    if (i != skipDeletedTask) {
+      taskStatusList = taskStatusList + document.getElementById('task-' + i).className + '|';
+      taskList = taskList + document.getElementById('task-' + i).innerText + '|';
+    }
+  }
+
+  if (taskCount == 1 && skipDeletedTask !== -1) {
+    // This condition is relevant when deleting the last remaining item
+    // Do nothing local storage has already been cleared
+  }
+  else {
+    taskStatusList = taskStatusList.substring(0, taskStatusList.length-1); // Remove last '|'
+    taskList = taskList.substring(0, taskList.length-1);
+    localStorage.setItem('CompletionStatus', taskStatusList);
+    localStorage.setItem('ListOfTasks', taskList);
+  }
+
+}
+
+
+
+// -------------------- Clear button -------------------- //
+var clear = document.getElementById('btn-clear')
 clear.onclick = function() {
   localStorage.removeItem('ListOfTasks');
   localStorage.removeItem('CompletionStatus');
+  window.location.reload(false); 
 }
-// ========================================================== //
+
 
 
 // -------------------- Modal Window -------------------- //
@@ -105,16 +178,3 @@ window.onclick = function(event) {
   }
 }
 
-// Add new task to local storage. 
-// When modal window closes, table reloads from local storage.
-function addNewTask() {
-  let newTask = document.getElementById('textbox-task').value;
-  if (localStorage.getItem("ListOfTasks") === null) {
-    localStorage.setItem("ListOfTasks", newTask);
-    localStorage.setItem("CompletionStatus", 'status-incomplete');
-  }
-  else {
-    localStorage.setItem("ListOfTasks", localStorage.getItem("ListOfTasks") + '|' + newTask);
-    localStorage.setItem("CompletionStatus", localStorage.setItem("CompletionStatus") + '|' + 'status-incomplete');
-  }
-}
